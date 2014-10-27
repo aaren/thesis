36115e1488292c0842299647713c15ed9a2c576a
Aaron O'Leary <aaron.oleary@gmail.com>
2014-10-27 08:10:53 +0000

    remove dups

--- abbreviations.md
+++
@@ -1,4 +0,0 @@
-Acronym   **W**hat it **S**tands **F**or
---------- ------------------------------
-WLB       **W**e **L**ike **B**ees
-NEB       **N**ever **E**at **B**ees
--- mathjax-conf.js
+++
@@ -1,6 +0,0 @@
-<script type="text/x-mathjax-config">
-    MathJax.Hub.Config({
-        TeX: { equationNumbers: { autoNumber: "all" } }
-            // can use autonumber: 'AMS' 
-    });
-</script>
