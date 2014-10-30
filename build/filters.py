35434ab2cedaf53c823825e335ebbd1c92542227
Aaron O'Leary <aaron.oleary@gmail.com>
2014-10-30 11:07:09 +0000

    suppress input cells in latex filter

--- build/filters.py
+++ build/filters.py
@@ -2,6 +2,13 @@ import pandocfilters as pf
 import internalreferences as ir
 
 
+def suppress_input_cells(key, value, format, metadata):
+    """Suppresses code cells that have the attribute '.input'.
+    """
+    if format == 'latex' and key == 'CodeBlock' and 'input' in value[0][1]:
+        return pf.Null()
+
+
 def raw_equations(key, value, format, metadata):
     """Pass latex contained in $$...$$ to latex writer
     as raw latex.
@@ -12,6 +19,8 @@ def raw_equations(key, value, format, metadata):
         # is inline
         return pf.RawInline('latex', content)
 
+
 if __name__ == '__main__':
     refman = ir.ReferenceManager()
-    ir.toJSONFilter(refman.reference_filter + [raw_equations])
+    ir.toJSONFilter(refman.reference_filter +
+                    [raw_equations, suppress_input_cells])
