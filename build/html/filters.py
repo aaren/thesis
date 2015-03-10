3af35555404297fc33c485a50919e96c77b962d6
Aaron O'Leary <aaron.oleary@gmail.com>
2015-03-10 17:30:12 +0000

    collapsing input cells

--- build/html/filters.py
+++ build/html/filters.py
@@ -4,6 +4,13 @@ import os
 
 import internalreferences as ir
 import pandocfilters as pf
+from pandocattributes import PandocAttributes
+
+
+collapse_button = """
+<input type="checkbox" id="codeblock-{n}" class="toggle">
+    <label for="codeblock-{n}">Source code [{n}]</label>
+""".format
 
 
 def make_site_links(key, value, format, metadata):
@@ -18,8 +25,20 @@ def make_site_links(key, value, format, metadata):
             return pf.Image(caption, (link, title))
 
 
+def enclose_input_code(key, value, format, metadata):
+    """Enclose any input code in a div to allow collapsing."""
+    if key == 'CodeBlock':
+        div_attr = {'classes': ['collapsible']}
+        code_attr = PandocAttributes(value[0], format='pandoc')
+        # this will fail if the codeblock doesn't have 'n' set
+        button = pf.RawBlock('html', collapse_button(n=code_attr['n']))
+        code = pf.CodeBlock(*value)
+        content = [button, pf.Div(pf.attributes({}), [code])]
+        return pf.Div(pf.attributes(div_attr), content)
+
+
 if __name__ == '__main__':
     refman = ir.ReferenceManager()
-    ir.toJSONFilter([make_site_links] + refman.reference_filter)
+    ir.toJSONFilter([make_site_links, enclose_input_code] + refman.reference_filter)
     # ir.toJSONFilter(refman.reference_filter)
     # ir.toJSONFilter(make_site_links)
